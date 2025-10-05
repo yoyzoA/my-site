266ce11e92e75a8423eed4952c4e171c9dd74606
YoyzoA <yorgoassal@gmail.com>
2025-10-05 16:32:04 +0300

    uiyguy

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
